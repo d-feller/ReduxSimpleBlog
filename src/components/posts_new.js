df09ff175ed445a279a18b6c80895fe2e82e90e0
Dmitrij Feller <ing.d.feller@gmail.com>
2018-08-29 20:30:58 +0200

    add create fetch and delete post

--- src/components/posts_new.js
+++ src/components/posts_new.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { createPost } from '../actions';
 
 class PostsNew extends Component {
 	renderField(field) {
@@ -21,7 +23,9 @@ class PostsNew extends Component {
 		);
 	}
 	onSubmit(values) {
-		console.log(values);
+		this.props.createPost(values, () => {
+			this.props.history.push('/');
+		});
 	}
 	render() {
 		const { handleSubmit } = this.props;
@@ -65,4 +69,6 @@ function validate(values) {
 export default reduxForm({
 	validate,
 	form: 'PostsNewForm'
-})(PostsNew) ;
+})(
+	connect(null, { createPost })( PostsNew )
+);
--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,12 @@
 import _ from 'lodash';
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POSTS, FETCH_POST, DELETE_POST } from '../actions';
 
 export default function (state = {}, action) {
 	switch (action.type) {
+		case DELETE_POST:
+		case FETCH_POST:
+			return { ...state, [ action.payload.data.id ]: action.payload.data };
+			break;
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 			break;
